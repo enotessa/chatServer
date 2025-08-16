1ac5db7430f7c86cbc6773c958cd59b8488b26ba
Anastasiia <nastualock@gmail.com>
2025-08-16 21:08:31 +0300

    update ProfessionEnum

--- gpt-api/src/main/java/com/enotessa/gpt/enums/ProfessionEnum.java
+++ gpt-api/src/main/java/com/enotessa/gpt/enums/ProfessionEnum.java
@@ -4,7 +4,12 @@ public enum ProfessionEnum {
     JAVA_JUNIOR("Java Junior"),
     JAVA_MIDDLE("Java Middle"),
     JAVA_SENIOR("Java Senior"),
-    KOTLIN_JUNIOR("Kotlin Junior");
+    KOTLIN_JUNIOR("Kotlin Junior"),
+    KOTLIN_MIDDLE("Kotlin Middle"),
+    KOTLIN_SENIOR("Kotlin Senior"),
+    IOS_JUNIOR("Ios Swift Junior"),
+    IOS_MIDDLE("Ios Swift Middle"),
+    IOS_SENIOR("Ios Swift Senior");
 
     private final String displayName;
 
