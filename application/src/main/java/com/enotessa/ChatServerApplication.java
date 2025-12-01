5ec0edf6edc4665616c92d362e05338231579cea
Anastasiia <nastualock@gmail.com>
2025-12-01 19:31:00 +0300

    .

--- application/src/main/java/com/enotessa/ChatServerApplication.java
+++ application/src/main/java/com/enotessa/ChatServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.enotessa.repositories")  // Важно!
+@EnableJpaRepositories("com.enotessa.repositories")
 public class ChatServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatServerApplication.class, args);
